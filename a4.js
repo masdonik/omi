c6ad562e1b3577b6b5333c3077ea6b8166be8cfc
masdonik <125558669+masdonik@users.noreply.github.com>
2024-08-13 19:17:22 +0700

    Create a4.js

---
+++ a4.js
@@ -0,0 +1,25 @@
+function autoRelated(data) {
+    var out = data[1];
+    var dom = '';
+
+    for (var i = 0; i < out.length; i++) {
+        var kw = out[i];
+        console.log("Processing keyword:", kw); // Debugging
+        dom += `<aside>
+                    <img alt="${kw}" src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}" 
+                         width="320" height="320" style="width:100%;height:auto;margin-right: 8px;margin-bottom: 8px;" />
+                    <small>Title: <i>${kw}</i></small>
+                    <h3>
+                        <a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a>
+                    </h3>
+                    <p align="justify">
+                        <b>${kw}</b> is among the best results for your search query <b>${postTitle}</b>. 
+                        The content provided here is purely for reference purposes. You are encouraged to save and bookmark the information on <b>${kw}</b> 
+                        for future use. We hope this serves as a valuable resource for your needs.
+                    </p>
+                </aside>`;
+    }
+
+    console.log("Generated DOM:", dom); // Debugging
+    document.getElementById('related-content').innerHTML = dom;
+}
